c66e40b0abdc8d06b49d5936f45cf552c9d8a361
Alexander Manzi <amanzi@CNYCN-MacBookPro-005.lan>
2023-01-10 21:19:41 -0500

    Update to react router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { HashRouter } from 'react-router-dom'
 import ScrollToTop from './components/ScrollToTop';
 import './index.css';
@@ -7,9 +8,9 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <ScrollToTop />
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
 
