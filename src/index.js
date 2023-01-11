c8033559d22119dc2f982d9eac9e9229fee2f290
Alexander Manzi <amanzi@CNYCN-MacBookPro-005.lan>
2023-01-10 19:58:52 -0500

    Switch to hashrouter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'
 import ScrollToTop from './components/ScrollToTop';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <ScrollToTop />
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 
