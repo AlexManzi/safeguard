686fdacd7bbc2600ce4dc6b9b38a773be523375d
Alexander Manzi <amanzi@CNYCN-MacBookPro-005.lan>
2023-01-10 21:24:39 -0500

    Updates to routing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,12 +24,12 @@ function Q() {
 
 
   let showServices =
-    <div className='items' id="socials"><HashLink to="/home/#ServicesWeOffer" className='linkFormat'><h1>Services Offered</h1></HashLink>
+    <div className='items' id="socials"><HashLink to="/#ServicesWeOffer" className='linkFormat'><h1>Services Offered</h1></HashLink>
       <div className='dropdown-menu'>
-        <li><HashLink to="/home/#PreSurvey" className='socialItems'><h4>Preconstruction Survey</h4></HashLink></li>
-        <li><HashLink to="/home/#VibrationMonitoring" className='socialItems'><h4>Vibration Monitoring</h4></HashLink></li>
-        <li><HashLink to="/home/#OpticalMonitoring" className='socialItems'><h4>Optical Monitoring</h4></HashLink></li>
-        <li><HashLink to="/home/#CrackMonitoring" className='socialItems'><h4>Crack Monitoring</h4></HashLink></li>
+        <li><HashLink to="/#PreSurvey" className='socialItems'><h4>Preconstruction Survey</h4></HashLink></li>
+        <li><HashLink to="/#VibrationMonitoring" className='socialItems'><h4>Vibration Monitoring</h4></HashLink></li>
+        <li><HashLink to="/#OpticalMonitoring" className='socialItems'><h4>Optical Monitoring</h4></HashLink></li>
+        <li><HashLink to="/#CrackMonitoring" className='socialItems'><h4>Crack Monitoring</h4></HashLink></li>
       </div>
 
     </div>
@@ -37,7 +37,7 @@ function Q() {
   return (
     <div id="wholebar">
       <div id="leftside">
-        <Link to="/home" className='linkFormat'><h1>Safeguard</h1></Link>
+        <Link to="/" className='linkFormat'><h1>Safeguard</h1></Link>
       </div>
       <nav>
       {(menu || screenWidth > 500) && (
